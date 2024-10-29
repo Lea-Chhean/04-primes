6dc1dec5fbddc279d0a37c822e6d5bb4035a5da9
Lea Chhean <lea.chhean@edu.esiee.fr>
2024-10-29 19:25:12 +0000

    Voici l'exercice Primes du cours 4

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+"""ce programme permet de vérifier si un nombre entier est premier ou pas"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    """ Fonction vérifiant si un entier p est premier"""
+    if p==1:
+        return False
+    for i in range(2, int(sqrt(p))+1):
+        if p%i==0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    """ Fonction principale affichant les nombres entiers de 0 à 99"""
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
